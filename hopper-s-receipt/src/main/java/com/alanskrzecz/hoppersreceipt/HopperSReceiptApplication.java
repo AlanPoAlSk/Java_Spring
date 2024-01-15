cf503e363caa338b8bfce7058a060bcf764b10f0
AlanPoAlsk <alsk91@gmail.com>
2024-01-15 16:10:56 -0500

    add hopper_s receipt assignment

---
+++ hopper-s-receipt/src/main/java/com/alanskrzecz/hoppersreceipt/HopperSReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.alanskrzecz.hoppersreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopperSReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopperSReceiptApplication.class, args);
+	}
+
+}
